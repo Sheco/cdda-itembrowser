caf8cb7cabadad330f1207d8922ceab45c826786
Sergio Duran <sduran@estilofusion.com>
2013-12-10 15:10:55 -0600

    Wider and better search results

--- app/views/items/search.blade.php
+++ app/views/items/search.blade.php
@@ -1,14 +1,18 @@
 @section('content')
 <div class="row">
-<div class="col-xs-4">
+<div class="col-xs-8">
 <h3>{{ $search }} </h3>
-<table class="table table-striped table-bordered">
+<table class="table table-bordered">
 <tr>
   <th>Name</th>
+  <th>can be crafted</th>
+  <th>can be used to craft</th>
 </tr>
 @foreach ($items as $item)
 <Tr>
   <td><a href="{{ route("item.view", ["id"=>$item->id]) }}">{{$item->prettyName}}</a></td>
+  <td>{{ count($item->recipes) }}</td>
+  <td>{{ count($item->toolFor) }}</td>
 </tr>
 @endforeach
 </table>

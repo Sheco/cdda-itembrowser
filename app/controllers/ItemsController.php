0afe58e8e6b13ccea26daf6809d775dbe6593f84
Sergio Duran <sduran@estilofusion.com>
2013-12-11 17:56:34 -0600

    By default show the recipes for the first available category.

--- app/controllers/ItemsController.php
+++ app/controllers/ItemsController.php
@@ -29,8 +29,15 @@ class ItemsController extends BaseController
   public function recipes($id, $category="")
   {
     $item = Items::get($id);
+    $categories = $item->toolCategories;
+    if($category=="")
+    {
+      $category = $categories[0];
+      error_log("use first category, $category");
+    }
     $recipes = $item->getToolForCategory($category);
-    return View::make('items.recipes', compact('item', "category", "recipes"));
+
+    return View::make('items.recipes', compact('item', "category", "recipes", "categories"));
   }
 
   public function disassemble($id)

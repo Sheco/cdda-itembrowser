ab2aec3f7139bbe7219bc41ccda931752491185f
Sergio Duran <sduran@estilofusion.com>
2014-01-21 11:15:18 -0600

    The condition to check if the recipe had books that thaught it had an error.

--- app/views/items/craft.blade.php
+++ app/views/items/craft.blade.php
@@ -44,7 +44,7 @@
     <br>
   @endforeach
   @endif
-@if ($recipe->book_learn)
+@if ($recipe->book_learn!="N/A")
 This recipe can be learned reading the following books:<br>
 @foreach($recipe->book_learn as $book)
   <a href="{{ route('item.view', $book[0]) }}">{{ Items::get($book[0])->name }} ({{ $book[1] }})</a><br>

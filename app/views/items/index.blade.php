1cbea30ea13883a9c15e438717f6d72298eaab71
Sergio Duran <sduran@estilofusion.com>
2014-02-11 11:51:04 -0600

    Improve home page text.

--- app/views/items/index.blade.php
+++ app/views/items/index.blade.php
@@ -6,10 +6,6 @@ This is a simple tool to browse through the items and recipies available in Cata
 </p>
 
 <p>
-It's something of a mockup of an idea to improve the user interface.
-</p>
-
-<p>
 Please use the search field on the top right corner of this page to look for an item. You can check the 
 item's properties, then check how it can be crafted and even see what can craft using that item.
 </p>
@@ -27,8 +23,8 @@ and
 
 <p>
 You can also search by the item's symbol, for example 
-{{link_to_route('item.search', 'Food', array("q"=>"%")) }}, 
-{{link_to_route('item.search', 'Books', array("q"=>"?"))}}
+{{link_to_route('item.search', '% is Food', array("q"=>"%")) }}, 
+{{link_to_route('item.search', '? are Books', array("q"=>"?"))}}
 </p>
 
 
@@ -36,8 +32,9 @@ You can also search by the item's symbol, for example
 Crafting could be as "simple" as looking at your hammer and being able to see what you can do with it.
 </p>
 
+<hr>
 <p>
-Beware, this is just a prototype, it's definitely not optimized, nor thoroughly tested.
+If you have any requests or if you find a bug, please let me know at <a href="mailto:sduran@estilofusion.com">sduran@estilofusion.com</a>
 </p>
 
 @stop

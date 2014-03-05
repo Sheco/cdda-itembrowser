16b6aa2aeef4e1e5a804760d77a441443961ce58
Sergio Duran <sduran@estilofusion.com>
2014-03-04 21:47:13 -0600

    Remove the search's list decoration.

--- app/views/items/search.blade.php
+++ app/views/items/search.blade.php
@@ -2,7 +2,7 @@
 <div class="row">
 <div class="col-xs-8">
 <h3>{{ $search }} </h3>
-<ul>
+<ul style="list-style: none; padding-left: 0px;">
 @foreach ($items as $item)
 <li>
 <a href="{{ route("item.view", array("id"=>$item->id)) }}">{{$item->name}}</a> 

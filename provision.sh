2741080ee0d6125f80a2345afa363cafc7a4863f
Sergio Duran <sduran@estilofusion.com>
2014-03-06 21:09:02 -0600

    Split the provision script in two.
    provision.sh is only to configure the system.
    setup.sh is to configure the app.

--- provision.sh
+++ provision.sh
@@ -1,45 +1,30 @@
-#!/bin/bash
+#!/bin/sh
 # tested on a debian 7.3 box
+# this is script would normally be ran only once per server
+# it downloads the system dependencies needed for the app
+
+
 # get the absolute path to the data files
 BASE_PATH=$(cd $(dirname $0 ) && pwd )
-STORAGE_PATH=/var/cache/cataclysm
 USER=vagrant
-WWW_USER=www-data
+DATA_PATH=/var/cache/cataclysm
 
-# if this script is run with one argument, that'll be the storage path.
-[ "$1" != "" ] && STORAGE_PATH=$1
-
-# edit some files, set the appropiate paths
-sed -i "s@require\s\+.*vendor/autoload.php.*@require '$STORAGE_PATH/vendor/autoload.php';@" $BASE_PATH/src/bootstrap/autoload.php
-sed -i "s@'storage'\s\+=>.*@'storage' => '$STORAGE_PATH/storage',@" $BASE_PATH/src/bootstrap/paths.php
-sed -i "s@\$framework\s\+=.*@\$framework = '$STORAGE_PATH/vendor/laravel/framework/src';@" $BASE_PATH/src/bootstrap/start.php
-sed -i "s@\"vendor-dir\"\s*:\s*\"[^\"]*\"@\"vendor-dir\": \"$STORAGE_PATH/vendor\"@" $BASE_PATH/src/composer.json
+# exit on error
+set -e
 
 # download packages
 apt-get update
 apt-get -y install php5 php5-mcrypt php5-mysql avahi-daemon php-apc unzip
 
 # setup apache2
-cp $BASE_PATH/sites-available/* /etc/apache2/sites-available
+cp "$BASE_PATH"/sites-available/* /etc/apache2/sites-available
 a2ensite cataclysm
 a2enmod rewrite
 service apache2 restart
-
-# make storage paths
-mkdir -p $STORAGE_PATH/{vendor,storage/{cache,logs,meta,sessions,views}}
-chmod -R g+w $STORAGE_PATH
-chown $USER:$WWW_USER -R $STORAGE_PATH
-
-# download the cataclysm dda's source code
-cd $BASE_PATH
-[ ! -e master.zip ] && wget https://github.com/CleverRaven/Cataclysm-DDA/archive/master.zip
-sudo -u $USER unzip -d $STORAGE_PATH/storage master.zip
 
 # download composer
 cd /usr/local/bin
 curl -sS https://getcomposer.org/installer | php -- --filename=composer
 
-# download php dependencies
-cd $BASE_PATH/src
-rm -f composer.lock
-sudo -u $USER composer install
+sudo -u $USER ./setup.sh "$DATA_PATH"
+chgrp -R www-data "$DATA_PATH"/storage/*
---
+++ setup.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+# exit on error
+set -e
+
+if test -z $1
+then
+  echo "Please provide the path for the data files"
+  exit 1
+fi
+# get the absolute path to the data files
+BASE_PATH=$(cd $(dirname $0 ) && pwd )
+DATA_PATH=$1
+
+cd "$BASE_PATH"
+# make storage paths
+mkdir -p "$DATA_PATH"/vendor
+for dir in cache logs meta sessions views
+do mkdir -p "$DATA_PATH"/storage/$dir
+done
+chmod -R g+w "$DATA_PATH"
+
+# download the cataclysm dda's source code
+[ ! -e master.zip ] && wget https://github.com/CleverRaven/Cataclysm-DDA/archive/master.zip
+unzip -d "$DATA_PATH" master.zip
+
+# edit some files, set the appropiate paths
+sed -i "s@require\s\+.*vendor/autoload.php.*@require '$DATA_PATH/vendor/autoload.php';@" src/bootstrap/autoload.php
+sed -i "s@'storage'\s\+=>.*@'storage' => '$DATA_PATH/storage',@" src/bootstrap/paths.php
+sed -i "s@\$framework\s\+=.*@\$framework = '$DATA_PATH/vendor/laravel/framework/src';@" src/bootstrap/start.php
+sed -i "s@\"vendor-dir\"\s*:\s*\"[^\"]*\"@\"vendor-dir\": \"$DATA_PATH/vendor\"@" src/composer.json
+
+
+# download php dependencies
+(
+cd src
+rm -f composer.lock
+composer install
+)
+
+echo "--------------------------"
+echo "You need to make sure the webserver can read/write to the storage path"
+echo ": $DATA_PATH/storage"

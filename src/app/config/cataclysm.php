fe5eb5f43f1c8d76ac85ff4921f2f290611d2f50
Sergio Duran <sduran@estilofusion.com>
2014-03-06 21:08:05 -0600

    CDDA's source code shouldn't be inside the storage path.

--- src/app/config/cataclysm.php
+++ src/app/config/cataclysm.php
@@ -1,7 +1,7 @@
 <?php
 
 return array(
-  "dataPath"=>storage_path().'/Cataclysm-DDA-master/data/json',
+  "dataPath"=>storage_path().'/../Cataclysm-DDA-master/data/json',
   "sites"=>array(
     "[stable]"=>"cdda.estilofusion.com",
     "[trunk]"=>"cdda-trunk.estilofusion.com"

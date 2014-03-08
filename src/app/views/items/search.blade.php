2bf71ebb6b6fac3340457a4b41a86337e7f527b6
Sergio Duran <sduran@estilofusion.com>
2014-03-08 10:06:20 -0600

    Tweak search result's html

--- src/app/views/items/search.blade.php
+++ src/app/views/items/search.blade.php
@@ -1,15 +1,13 @@
 @section('content')
-<div class="row">
-<div class="col-sm-4">
 <h3>{{ $search }} </h3>
-<ul style="list-style: none; padding-left: 0px;">
 @foreach ($items as $item)
-<li>
-<a href="{{ route("item.view", array("id"=>$item->id)) }}">{{$item->name}}</a> 
-{{ $item->featureLabels }}
-</li>
+<div class="row">
+
+<div class="col-md-6">
+  <a href="{{ route("item.view", array("id"=>$item->id)) }}">{{$item->name}}</a>
+  {{ $item->featureLabels }}
+</div>
+
+</div>
 @endforeach
-</ul>
-</div>
-</div>
 @stop

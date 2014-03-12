7e545f453fd81731951dd9fe27dca6fc6cb8eeb9
Sergio Duran <sduran@estilofusion.com>
2014-03-12 14:12:23 -0600

    There was some leftover pivot code.

--- src/app/services.php
+++ src/app/services.php
@@ -3,4 +3,3 @@ $app->singleton('RepositoryInterface', 'JsonRepository');
 $app->singleton('ItemRepositoryInterface', 'ItemRepositoryCache');
 $app->singleton('RecipeRepositoryInterface', 'RecipeRepositoryCache');
 $app->singleton('MaterialRepositoryInterface', 'MaterialRepositoryCache');
-$app->singleton('ItemRepositoryPivotInterface', 'ItemRepositoryPivotCache');

1e7e0ce7255bfd88982dcdcfd0dcfa1de6e2788e
Sergio Duran <sduran@estilofusion.com>
2014-03-12 17:56:29 -0600

    Index some common item types.

--- src/app/repositories/ItemRepository.php
+++ src/app/repositories/ItemRepository.php
@@ -19,10 +19,105 @@ class ItemRepository implements ItemRepositoryInterface, IndexerInterface
   public function getIndexes($object)
   {
     $indexes = array();
-    if(isset($this->types[$object->type]))
+    if(!isset($this->types[$object->type]))
+      return $indexes;
+    $indexes["item"] = $object->id;
+    if($object->type=="ARMOR" and !isset($object->covers)) 
     {
-      $indexes["item"] = $object->id;
+        $indexes["armor.none"] = $object->id;
+    } 
+    else if($object->type=="ARMOR" and isset($object->covers))
+    {
+      if(in_array("FEET", $object->covers))
+      {
+        $indexes["armor.feet"] = $object->id;
+      }
+      if(in_array("ARMS", $object->covers))
+      {
+        $indexes["armor.arms"] = $object->id;
+      }
+      if(in_array("EYES", $object->covers))
+      {
+        $indexes["armor.eyes"] = $object->id;
+      }
+      if(in_array("LEGS", $object->covers))
+      {
+        $indexes["armor.legs"] = $object->id;
+      }
+      if(in_array("HANDS", $object->covers))
+      {
+        $indexes["armor.hands"] = $object->id;
+      }
+      if(in_array("TORSO", $object->covers))
+      {
+        $indexes["armor.torso"] = $object->id;
+      }
+      if(in_array("HEAD", $object->covers))
+      {
+        $indexes["armor.head"] = $object->id;
+      }
+      if(in_array("MOUTH", $object->covers))
+      {
+        $indexes["armor.mouth"] = $object->id;
+      }
     }
+    if($object->type=="CONTAINER")
+      $indexes["container"] = $object->id;
+    if($object->type=="COMESTIBLE")
+      $indexes["food"] = $object->id;
+    if($object->type=="TOOL")
+      $indexes["tool"] = $object->id;
+    if($object->type=="BOOK")
+    {
+      if($object->skill=="none")
+      {
+        if($object->fun>0)
+          $indexes["book.entertainment"] = $object->id;
+        else
+          $indexes["book.boring"] = $object->id;
+      } else if(in_array($object->skill, array(
+        "archery", "handguns", "markmanship",
+        "launcher", "firearms", "throw", "rifle",
+        "shotgun", "smg", "pistol", "gun")))
+      {
+        $indexes["book.range"] = $object->id;
+      } 
+      else if(in_array($object->skill, array(
+        "bashing", "cutting", "stabbing", "dodge",
+        "melee", "unarmed")))
+      {
+        $indexes["book.combat"] = $object->id;
+      } 
+      else if(in_array($object->skill, array(
+        "computer", "electronics", "fabrication",
+        "mechanics", "construction", "carpentry",
+        "traps")))
+      {
+        $indexes["book.engineering"] = $object->id;
+      } 
+      else if(in_array($object->skill, array(
+        "cooking", "tailor", "firstaid")))
+      {
+        $indexes["book.crafts"] = $object->id;
+      } 
+      else if(in_array($object->skill, array(
+        "barter", "speech")))
+      {
+        $indexes["book.social"] = $object->id;
+      } 
+      else if(in_array($object->skill, array(
+        "driving", "survival", "swimming")))
+      {
+        $indexes["book.survival"] = $object->id;
+      } 
+      else 
+      {
+        $indexes["book.other"] = $object->id;
+      }
+      
+
+    }
+
     //TODO: check extra indexes (armor, melee, books, etc)
     return $indexes;
   }

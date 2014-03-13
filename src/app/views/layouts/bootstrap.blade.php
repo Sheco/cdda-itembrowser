703ed9051d1b430fbc9da673349670e8ab8e0ece
Sergio Duran <sduran@estilofusion.com>
2014-03-12 21:22:27 -0600

    Change fonts.

--- src/app/views/layouts/bootstrap.blade.php
+++ src/app/views/layouts/bootstrap.blade.php
@@ -15,7 +15,7 @@
     <!-- Custom styles for this template -->
     <link href="/css/starter-template.css" rel="stylesheet">
 
-    <link href="/css/cataclysm.css?v=1" rel="stylesheet">
+    <link href="/css/cataclysm.css?v=2" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>

a11643522c44a7fbb9aa6abf7f5e5de0468d7ee9
Sergio Duran <sduran@estilofusion.com>
2014-03-13 14:42:36 -0600

    Register repositories in start/global instead of services.php

--- src/app/services.php
+++
@@ -1,5 +0,0 @@
-<?php
-$app->singleton('Repositories\Recipe', 'Repositories\Recipe');
-$app->singleton('Repositories\Item', 'Repositories\Item');
-$app->singleton('Repositories\Material', 'Repositories\Material');
-$app->singleton('Repositories\RepositoryInterface', 'Repositories\JsonCache');
--- src/app/start/global.php
+++ src/app/start/global.php
@@ -81,4 +81,9 @@ App::down(function()
 */
 
 require app_path().'/filters.php';
-require app_path().'/services.php';
+
+// register singleton repositories
+$app->singleton('Repositories\Recipe', 'Repositories\Recipe');
+$app->singleton('Repositories\Item', 'Repositories\Item');
+$app->singleton('Repositories\Material', 'Repositories\Material');
+$app->singleton('Repositories\RepositoryInterface', 'Repositories\JsonCache');

91dfe09bc5c7d301aa38b0970fa306e129462990
Sergio Duran <sduran@estilofusion.com>
2014-03-12 22:46:25 -0600

    Manage the repository item IDs internally.

--- src/app/repositories/JsonRepository.php
+++ src/app/repositories/JsonRepository.php
@@ -4,9 +4,11 @@ class JsonRepository implements RepositoryInterface
 {
   protected $database;
   protected $index;
+  private $id;
 
   function __construct()
   {
+    $this->id = 0;
     $this->index = array();
     $this->database = array();
   }
@@ -25,16 +27,15 @@ class JsonRepository implements RepositoryInterface
     $this->index[$index][$key] = $object->repo_id;
   }
 
-  private function newObject($id, $object)
+  private function newObject($object)
   {
-    $object->repo_id = $id;
-    $this->database[$id] = $object;
+    $object->repo_id = $this->id++;
+    $this->database[$object->repo_id] = $object;
     Event::fire("cataclysm.newObject", array($this, $object));
   }
 
   protected function read()
   {
-    $id = 0;
     $this->database = array();
     $it = new RecursiveDirectoryIterator(\Config::get("cataclysm.dataPath"));
     error_log("Reading data files...");
@@ -43,11 +44,11 @@ class JsonRepository implements RepositoryInterface
       $data = (array) json_decode(file_get_contents($file));
       foreach($data as $object) 
       {
-        $this->newObject($id++, $object);
+        $this->newObject($object);
       }
     }
-    $this->newObject($id++, json_decode('{"id":"toolset","name":"integrated toolset","type":"_SPECIAL"}'));
-    $this->newObject($id++, json_decode('{"id":"fire","name":"nearby fire","type":"_SPECIAL"}'));
+    $this->newObject(json_decode('{"id":"toolset","name":"integrated toolset","type":"_SPECIAL"}'));
+    $this->newObject(json_decode('{"id":"fire","name":"nearby fire","type":"_SPECIAL"}'));
   }
 
   // return a single object

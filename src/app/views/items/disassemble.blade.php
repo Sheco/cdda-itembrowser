da8d7e66c417458f4a702ea973d034ec86f9fc96
Sergio Duran <sduran@estilofusion.com>
2014-03-13 19:24:39 -0600

    Clean disassemble window.

--- src/app/views/items/disassemble.blade.php
+++ src/app/views/items/disassemble.blade.php
@@ -12,7 +12,7 @@
 @include('items.menu', array('active'=>'disassemble'))
   <a href="{{ route("item.view", array("id"=>$item->id)) }}">{{ $item->name }}</a>
 @if (count($item->disassembly)>0)
- can be disassembled to obtain the components in this recipe.<br>
+ can be disassembled to obtain the following components.<br>
 @else
  can't be disassembled.
 @endif
@@ -20,20 +20,10 @@
 <div class="row">
 <div class="col-md-6">
 @foreach ($item->disassembly as $recipe)
-  {{link_to_route("item.view", $recipe->result->name, array("id"=>$recipe->result->id))}}<br>
-  Skill used: {{{ $recipe->skill_used }}} <br>
-  Required skills: {{ $recipe->skillsRequired }} <br>
-  Difficulty: {{{ $recipe->difficulty }}}<br>
-  Reversible: {{{ $recipe->reversible }}}<br>
-  Learn Automatically: {{{ $recipe->autolearn }}}<br>
-  Time to complete: {{{ $recipe->time }}}<br>
-  @if ($recipe->hasTools)
-  {{$recipe->tools}}<br>
-  @endif
-
   @if ($recipe->hasComponents)
   {{$recipe->components}}<br>
   @endif
+  --<br>
 <br>
 @endforeach
 </div>

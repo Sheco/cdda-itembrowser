f5a93c2416cf592ad90037d964dae079d3e02325
Sergio Duran <sduran@estilofusion.com>
2014-03-23 19:44:57 -0600

    Remove obsolete options.

--- src/app/config/cataclysm.php
+++ src/app/config/cataclysm.php
@@ -1,19 +1,11 @@
 <?php
 
 return array(
-  "dataPath"=>storage_path().'/../Cataclysm-DDA-master/data/json',
   "sites"=>array(
     "stable"=>"cdda.estilofusion.com",
     "development"=>"cdda-trunk.estilofusion.com"
   ),
-  // the object and index database will be split in chunks...
-  // for memcache and apc storage, a small number is good
-  // but for disk cache, it's better to avoid lots of very small files.
-  // a value of 1 would create about 13000 cache entries.
-  // a value of 100 would create about 300 cache entries.
-  "itemsPerCache"=>100,
 
-  "dataCacheExpiration"=>60,
   "searchCacheExpiration"=>60
 
 );

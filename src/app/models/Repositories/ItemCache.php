256c6b380c9ad064190813ea0d191d98a22451d0
Sergio Duran <sduran@estilofusion.com>
2014-03-23 22:24:45 -0600

    Replace underscores to spaces to avoid clobbering searches. Otherwise, searching for "wooden_arrow" would store an empty result, which will be returned when searching for "wooden arrow"

--- src/app/models/Repositories/ItemCache.php
+++ src/app/models/Repositories/ItemCache.php
@@ -6,6 +6,7 @@ class ItemCache extends Item
   const CACHE_KEY = "itemSearch";
   public function where($text)
   {
+    $text = str_replace("_", " ", $text);
     $key = self::CACHE_KEY.":".str_replace(" ", "_", $text);
     $expiration = \Config::get("cataclysm.searchCacheExpiration");
     $items = \Cache::remember($key, $expiration, function () use ($text) {

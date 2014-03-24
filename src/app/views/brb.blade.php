8f15c251a86aec3ec92a729d048d8466a8647d16
Sergio Duran <sduran@estilofusion.com>
2014-03-23 20:10:50 -0600

    Better looking maintenance page.

---
+++ src/app/views/brb.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.bootstrap')
+@section('title')
+Maintenance mode
+@endsection
+@section('content')
+The browser is temporarily unavailable as we run some maintentance routines.<br>
+<br>
+Please try again in a couple of minutes.
+@endsection

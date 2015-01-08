7a40423f90d8042d5ff76e5fdcf652c696ef714d
Sergio Duran <sduran@estilofusion.com>
2015-01-08 00:24:03 -0600

    Remove unneeded monster indexer code.

--- src/app/models/Repositories/Indexers/MonsterGroup.php
+++ src/app/models/Repositories/Indexers/MonsterGroup.php
@@ -12,9 +12,6 @@ class MonsterGroup implements IndexerInterface
 
     public function onFinishedLoading(LocalRepository $repo)
     {
-        foreach ($repo->all(self::DEFAULT_INDEX) as $id) {
-            $group = $repo->get(self::DEFAULT_INDEX, $id);
-        }
     }
 
     public function onNewObject(LocalRepository $repo, $object)
@@ -22,15 +19,6 @@ class MonsterGroup implements IndexerInterface
         if ($object->type == "monstergroup") {
             $repo->set(self::DEFAULT_INDEX, $object->name);
             $repo->set(self::DEFAULT_INDEX.".".$object->name, $object->repo_id);
-
-            // create unique monsters array
-            $monsters = array();
-
-            foreach ($object->monsters as $monster) {
-                $monster = $monster->monster;
-                $monsters[$monster] = true;
-            }
-            $object->uniqueMonsters = array_keys($monsters);
 
             return;
         }

b6833d82fd1f4363dd676a7febde785d5a44db4c
Sergio Duran <sduran@estilofusion.com>
2015-01-11 23:11:33 -0600

    Improve the maintenance page.

--- src/app/views/brb.blade.php
+++ src/app/views/brb.blade.php
@@ -1,6 +1,12 @@
 @section('title')
 Maintenance mode
 @endsection
-The browser is temporarily unavailable as we run some maintentance routines.<br>
+The Item Browser is temporarily unavailable as we run some maintentance routines.<br>
 <br>
+It might be for a few reasons:<br>
+<ul>
+<li>An update requiring to recompile the database.</li>
+<li>A more complicated update requiring some maintenance tasks.</li>
+<li>An automatic recompilation of the database (dev nightly update).</li>
+</ul>
 Please try again in a couple of minutes.

2d207cf65d944141e52a7df979e18e5806c11ffd
Sergio Duran <sduran@estilofusion.com>
2015-01-14 13:38:34 -0600

    Add source to the construction table.

--- src/app/views/world/constructionCategories.blade.php
+++ src/app/views/world/constructionCategories.blade.php
@@ -18,16 +18,24 @@ Construction categories - Cataclysm: Dark Days Ahead
 <tr>
     <th>Construction</th>
     <th></th>
+    <th>Source</th>
+    <th></th>
     <th>Result</th>
 </tr>
 </thead>
 @foreach($data as $d) 
 <tr>
-    <td>{{ link_to_route("construction.view", $d->description, $d->repo_id); }}
-@if($d->comment!="")
-({{{$d->comment}}})
+    <td>{{ link_to_route("construction.view", $d->description, $d->repo_id); }}</td>
+@if ($d->has_pre_terrain)
+    <td>{{$d->pre_terrain->symbol}}</td>
+    <td>{{$d->pre_terrain->name}}</td>
+@elseif ($d->pre_flags)
+    <td></td>
+    <td>is:{{$d->pre_flags}}</td>
+@else
+    <td></td>
+    <td></td>
 @endif
-    </td>
 @if ($d->has_post_terrain) 
     <td>{{$d->post_terrain->symbol}}</td>
     <td>{{$d->post_terrain->name}}</td>

4489e8f65a22f5ae3b3e1412871ca560834ad4bb
Sergio Duran <sduran@estilofusion.com>
2015-01-14 13:21:25 -0600

    Colorize construction result symbols.

--- src/app/models/Presenters/Furniture.php
+++ src/app/models/Presenters/Furniture.php
@@ -3,5 +3,10 @@ namespace Presenters;
 
 class Furniture extends \Robbo\Presenter\Presenter
 {
+    public function presentSymbol()
+    {
+        list($fg, $bg) = colorPairToCSS($this->object->color);
+        return "<span style=\"color: $fg; background: $bg\">{$this->object->symbol}</span>";
+    }
 }
 
--- src/app/models/Presenters/Terrain.php
+++ src/app/models/Presenters/Terrain.php
@@ -3,4 +3,9 @@ namespace Presenters;
 
 class Terrain extends \Robbo\Presenter\Presenter
 {
+    public function presentSymbol()
+    {
+        list($fg, $bg) = colorPairToCSS($this->object->color);
+        return "<span style=\"color: $fg; background: $bg\">{$this->object->symbol}</span>";
+    }
 }

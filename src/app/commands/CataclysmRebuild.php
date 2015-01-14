0363cd9806af1d871e6ab7a0bba3f3869e621060
Sergio Duran <sduran@estilofusion.com>
2015-01-14 10:57:20 -0600

    Construction indexer has to go first.
    The item indexer counts the number of constructions the item has,
    but the construction quality indexer has to be run after all the
    items have been loaded.
    
    When the item indexer checks the counts, the construction indexer
    hasn't finished it's job.

--- src/app/commands/CataclysmRebuild.php
+++ src/app/commands/CataclysmRebuild.php
@@ -78,6 +78,7 @@ class CataclysmCache extends Command
     {
         //TODO: find a way to find the indexers automatically
         // instead of hardcoding them here.
+        $this->registerIndexer(new Indexers\Construction);
         $this->registerIndexer(new Indexers\Item());
         $this->registerIndexer(new Indexers\Material());
         $this->registerIndexer(new Indexers\Recipe());
@@ -85,7 +86,6 @@ class CataclysmCache extends Command
         $this->registerIndexer(new Indexers\Monster());
         $this->registerIndexer(new Indexers\MonsterGroup());
         $this->registerIndexer(new Indexers\Terrain);
-        $this->registerIndexer(new Indexers\Construction);
     }
 
     private function registerIndexer(Indexers\IndexerInterface $indexer)

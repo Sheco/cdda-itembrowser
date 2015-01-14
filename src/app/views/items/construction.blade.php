730f23db4e4fa9797678656e78f0ec3652b28ce5
Sergio Duran <sduran@estilofusion.com>
2015-01-14 13:46:03 -0600

    Integrate the same construction table to the items.

--- src/app/views/items/construction.blade.php
+++ src/app/views/items/construction.blade.php
@@ -7,13 +7,6 @@
 </h1>
 <div class="row">
 <div class="col-md-6">
-@foreach ($item->constructionUses as $construction)
-{{ link_to_route("construction.view", $construction->description, array($construction->id)) }}
-@if($construction->comment!="")
-({{ $construction->comment }})
-@endif
-
-<br>
-@endforeach
-
+@include("world._constructionList", array("data"=>$item->constructionUses))
+</div>
 </div>
--- src/app/views/world/constructionCategories.blade.php
+++ src/app/views/world/constructionCategories.blade.php
@@ -13,39 +13,7 @@ Construction categories - Cataclysm: Dark Days Ahead
   </div>
 
   <div class="col-md-9">
-<table class="table table-bordered">
-<thead>
-<tr>
-    <th>Construction</th>
-    <th></th>
-    <th>Source</th>
-    <th></th>
-    <th>Result</th>
-</tr>
-</thead>
-@foreach($data as $d) 
-<tr>
-    <td>{{ link_to_route("construction.view", $d->description, $d->repo_id); }}</td>
-@if ($d->has_pre_terrain)
-    <td>{{$d->pre_terrain->symbol}}</td>
-    <td>{{$d->pre_terrain->name}}</td>
-@elseif ($d->pre_flags)
-    <td></td>
-    <td>is:{{$d->pre_flags}}</td>
-@else
-    <td></td>
-    <td></td>
-@endif
-@if ($d->has_post_terrain) 
-    <td>{{$d->post_terrain->symbol}}</td>
-    <td>{{$d->post_terrain->name}}</td>
-@else
-    <td></td>
-    <td></td>
-@endif
-</tr>
-@endforeach
-</table>
+  @include("world._constructionList", array("data"=>$data))
   </div>
 </div>
 

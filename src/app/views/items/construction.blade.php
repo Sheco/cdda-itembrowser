05374c015cd093cb34328295d6042b2b68ab4a85
Sergio Duran <sduran@estilofusion.com>
2015-01-14 18:52:51 -0600

    Increase the width of the construction table in the item view.

--- src/app/views/items/construction.blade.php
+++ src/app/views/items/construction.blade.php
@@ -5,8 +5,4 @@
 <h1>
   {{$item->symbol}} <a href="{{ route("item.view", array("id"=>$item->id)) }}">{{ $item->name }}</a> construction.
 </h1>
-<div class="row">
-<div class="col-md-6">
 @include("world._constructionList", array("data"=>$item->constructionUses))
-</div>
-</div>

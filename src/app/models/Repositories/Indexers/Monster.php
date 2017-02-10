e82e3fb0b4fd71f44bd0cf99f9a0c823fbfd53c7
Sergio Duran <sduran@localhost.localdomain>
2017-02-10 12:55:31 -0600

    Some monsters don't have IDs, ignore them.

--- src/app/models/Repositories/Indexers/Monster.php
+++ src/app/models/Repositories/Indexers/Monster.php
@@ -11,16 +11,24 @@ class Monster implements IndexerInterface
 
     public function onNewObject(RepositoryWriterInterface $repo, $object)
     {
-        if ($object->type == "MONSTER") {
-            $repo->append(self::DEFAULT_INDEX, $object->id);
-            $repo->set(self::DEFAULT_INDEX.".".$object->id, $object->repo_id);
-            $objspecies = (array) $object->species;
-            foreach ($objspecies as $species) {
-                $repo->append("monster.species.$species", $object->id);
-                $repo->addUnique("monster.species", $species);
-            }
+            if ($object->type == "MONSTER") {
 
-            return;
+                // skip objects without an ID    
+                if(!isset($object->id)) return;
+
+                $repo->append(self::DEFAULT_INDEX, $object->id);
+                $repo->set(self::DEFAULT_INDEX.".".$object->id, $object->repo_id);
+
+                // define a default "none" species
+                if(!isset($object->species))  $object->species = "none";
+
+                $objspecies = (array) $object->species;
+                foreach ($objspecies as $species) {
+                    $repo->append("monster.species.$species", $object->id);
+                    $repo->addUnique("monster.species", $species);
+                }
+
+                return;
         }
     }
 
